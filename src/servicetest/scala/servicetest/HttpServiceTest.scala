2b99637a976232f67355b597c3ff62f3a3a04d35
Laurence Bird <laurence.bird@ovoenergy.com>
2018-05-16 14:38:12 +0100

    Refactor tests to accomodate new template structure

--- src/servicetest/scala/servicetest/HttpServiceTest.scala
+++ src/servicetest/scala/servicetest/HttpServiceTest.scala
@@ -2,8 +2,6 @@ package servicetest
 
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths}
-
-import cats.data.Kleisli
 import io.circe._
 import io.circe.generic.semiauto._
 import io.circe.syntax._
@@ -12,9 +10,7 @@ import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.{AmazonS3Client, S3ClientOptions}
 import com.ovoenergy.comms.composer.http.RenderRestApi.{ErrorResponse, RenderRequest, RenderResponse}
-import com.typesafe.config.{Config, ConfigFactory}
 import org.scalatest.{Failed => _, _}
-
 import scala.concurrent.duration._
 import scala.language.reflectiveCalls
 import org.http4s.{Status, _}
@@ -25,24 +21,27 @@ import org.http4s.dsl.io._
 import org.http4s.circe._
 import org.http4s.client._
 import cats.implicits._
-import com.ovoenergy.comms.model.TemplateData
+import com.ovoenergy.comms.model.{CommManifest, TemplateData}
+import com.ovoenergy.comms.model.{Service => ServiceComm}
 import org.mockserver.model.HttpRequest.request
 import org.mockserver.model.HttpResponse.response
+import servicetest.util.MockTemplates
 
 class HttpServiceTest
     extends FlatSpec
     with Matchers
     with OptionValues
     with BeforeAndAfterAll
-    with DockerIntegrationTest {
-
-//  override val config: Config = ConfigFactory.load("servicetest.conf")
+    with DockerIntegrationTest
+    with MockTemplates {
 
   implicit val patience: PatienceConfig = PatienceConfig(5.minutes, 1.second)
 
   val pdfResponseByteArray = Files.readAllBytes(Paths.get("src/servicetest/resources/test.pdf"))
 
   val s3Endpoint = "http://localhost:4569"
+
+  val validTemplateCommManifest = CommManifest(ServiceComm, "canary", "1.0")
 
   lazy val s3Client = {
     val s3clientOptions = S3ClientOptions.builder().setPathStyleAccess(true).disableChunkedEncoding().build()
@@ -53,39 +52,14 @@ class HttpServiceTest
     s3
   }
 
+  private final val templatesBucket = "ovo-comms-templates"
+
   override def beforeAll() = {
     super.beforeAll()
-    uploadTemplateToS3()
-    createOKDocRaptorResponse()
-  }
-
-  private def uploadTemplateToS3(): Unit = {
-    // disable chunked encoding to work around https://github.com/jubos/fake-s3/issues/164
-
-    s3Client.createBucket("ovo-comms-templates")
+    s3Client.createBucket(templatesBucket)
     s3Client.createBucket("dev-ovo-comms-pdfs")
-
-    // template
-    s3Client.putObject("ovo-comms-templates",
-                       "service/composer-service-test/0.1/email/subject.txt",
-                       "SUBJECT {{profile.firstName}}")
-    s3Client.putObject("ovo-comms-templates",
-                       "service/composer-service-test/0.1/email/body.html",
-                       "{{> header}} HTML BODY {{amount}}")
-    s3Client.putObject("ovo-comms-templates",
-                       "service/composer-service-test/0.1/email/body.txt",
-                       "{{> header}} TEXT BODY {{amount}}")
-    s3Client.putObject("ovo-comms-templates",
-                       "service/composer-service-test/0.1/sms/body.txt",
-                       "{{> header}} SMS BODY {{amount}}")
-    s3Client.putObject("ovo-comms-templates",
-                       "service/composer-service-test/0.1/print/body.html",
-                       "Hello {{profile.firstName}}")
-
-    // fragments
-    s3Client.putObject("ovo-comms-templates", "service/fragments/email/html/header.html", "HTML HEADER")
-    s3Client.putObject("ovo-comms-templates", "service/fragments/email/txt/header.txt", "TEXT HEADER")
-    s3Client.putObject("ovo-comms-templates", "service/fragments/sms/txt/header.txt", "SMS HEADER")
+    uploadTemplateToS3(validTemplateCommManifest, s3Client, templatesBucket)
+    createOKDocRaptorResponse()
   }
 
   behavior of "Composer HTTP service"
@@ -118,7 +92,8 @@ class HttpServiceTest
 
     val req = Request[IO](
       Method.POST,
-      Uri.unsafeFromString(s"$composerHttpEndpoint/render/composer-service-test/0.1/Service/print"),
+      Uri.unsafeFromString(
+        s"$composerHttpEndpoint/render/${validTemplateCommManifest.name}/${validTemplateCommManifest.version}/Service/print"),
       body = fs2.Stream
         .emit(renderRequest.asJson)
         .flatMap(json => fs2.Stream.emits(json.noSpaces.getBytes(StandardCharsets.UTF_8).toSeq))
@@ -152,7 +127,8 @@ class HttpServiceTest
 
     val req = Request[IO](
       Method.POST,
-      Uri.unsafeFromString(s"$composerHttpEndpoint/render/composer-service-test/0.1/invalid/print"),
+      Uri.unsafeFromString(
+        s"$composerHttpEndpoint/render/${validTemplateCommManifest.name}/${validTemplateCommManifest.version}/invalid/print"),
       body = fs2.Stream
         .emit(renderRequest.asJson)
         .flatMap(json => fs2.Stream.emits(json.noSpaces.getBytes(StandardCharsets.UTF_8).toSeq))
@@ -180,7 +156,8 @@ class HttpServiceTest
 
     val req = Request[IO](
       Method.POST,
-      Uri.unsafeFromString(s"$composerHttpEndpoint/render/composer-service-test/0.1/Service/print"),
+      Uri.unsafeFromString(
+        s"$composerHttpEndpoint/render/${validTemplateCommManifest.name}/${validTemplateCommManifest.version}/Service/print"),
       body = fs2.Stream
         .emit(renderRequest.asJson)
         .flatMap(json => fs2.Stream.emits(json.noSpaces.getBytes(StandardCharsets.UTF_8).toSeq))
--- src/servicetest/scala/servicetest/PrintServiceTest.scala
+++ src/servicetest/scala/servicetest/PrintServiceTest.scala
@@ -3,6 +3,7 @@ package servicetest
 import java.nio.file.{Files, Paths}
 import java.time.format.DateTimeFormatter
 import java.time.{Instant, LocalDateTime, ZoneId}
+
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.model.ObjectListing
@@ -14,10 +15,10 @@ import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers, OptionValues}
 import com.ovoenergy.comms.testhelpers.KafkaTestHelpers._
 import com.ovoenergy.comms.helpers.Kafka
 import com.ovoenergy.comms.model._
-import com.typesafe.config.ConfigFactory
-import org.mockserver.client.server.MockServerClient
 import org.mockserver.model.HttpRequest.request
 import org.mockserver.model.HttpResponse.response
+import servicetest.util.MockTemplates
+
 import scala.collection.JavaConverters._
 import scala.language.reflectiveCalls
 //Implicits
@@ -25,14 +26,14 @@ import com.ovoenergy.comms.serialisation.Codecs._
 import org.scalacheck.Shapeless._
 import scala.concurrent.duration._
 import scala.concurrent.duration._
-import scala.io.{BufferedSource, Source}
 
 class PrintServiceTest
     extends FlatSpec
     with Matchers
     with OptionValues
     with BeforeAndAfterAll
-    with DockerIntegrationTest {
+    with DockerIntegrationTest
+    with MockTemplates {
 
   behavior of "Composer print service"
 
@@ -42,54 +43,6 @@ class PrintServiceTest
   val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
   val now = Instant.now()
   val pdfResponseByteArray = Files.readAllBytes(Paths.get("src/servicetest/resources/test.pdf"))
-
-  override def beforeAll(): Unit = {
-    super.beforeAll()
-    uploadTemplateToS3()
-  }
-
-  lazy val s3Client = {
-    val s3clientOptions = S3ClientOptions.builder().setPathStyleAccess(true).disableChunkedEncoding().build()
-    val s3: AmazonS3Client = new AmazonS3Client(new BasicAWSCredentials("service-test", "dummy"))
-      .withRegion(Regions.fromName(config.getString("aws.region")))
-    s3.setS3ClientOptions(s3clientOptions)
-    s3.setEndpoint(s3Endpoint)
-    s3
-  }
-
-  it should "create a ComposedPrint event, for a valid request, archiving the rendered pdf in s3" in {
-    uploadTemplateToS3()
-    createOKDocRaptorResponse()
-    withThrowawayConsumerFor(topics.composedPrint.v1) { consumer =>
-      topics.orchestratedPrint.v1.publishOnce(orchestratedPrintEvent, 10.seconds)
-      val composedPrintEvent = consumer.pollFor(noOfEventsExpected = 1).head
-      val key =
-        s"composer-service-test/${LocalDateTime.ofInstant(now, ZoneId.systemDefault()).format(dateFormatter)}/${now.toEpochMilli}-${orchestratedPrintEvent.metadata.traceToken}-${orchestratedPrintEvent.internalMetadata.internalTraceToken}.pdf"
-
-      composedPrintEvent.metadata.traceToken shouldBe orchestratedPrintEvent.metadata.traceToken
-      composedPrintEvent.internalMetadata.internalTraceToken shouldBe orchestratedPrintEvent.internalMetadata.internalTraceToken
-      composedPrintEvent.pdfIdentifier should be(key)
-      val s3 = s3Client
-
-      val files: ObjectListing = s3.listObjects("dev-ovo-comms-pdfs")
-      files.getObjectSummaries.asScala.foreach(println)
-
-      val s3Object = s3.getObject("dev-ovo-comms-pdfs", key)
-      IOUtils.toByteArray(s3Object.getObjectContent) should contain theSameElementsAs (pdfResponseByteArray)
-    }
-  }
-
-  it should "send a failed event if docRaptor is not available" in {
-    uploadTemplateToS3()
-    create400DocRaptorResponse()
-    withThrowawayConsumerFor(topics.failed.v2) { consumer =>
-      topics.orchestratedPrint.v1.publishOnce(orchestratedPrintEvent, 10.seconds)
-      val failedEvent = consumer.pollFor(noOfEventsExpected = 1).head
-
-      failedEvent.errorCode should be(CompositionError)
-      failedEvent.reason should be("Failed to render pdf: Bad Request")
-    }
-  }
 
   val accountNumber = TemplateData.fromString("11112222")
   val ovoId = TemplateData.fromString("myOvo999")
@@ -126,33 +79,54 @@ class PrintServiceTest
     Some(now.plusSeconds(100))
   )
 
-  private def uploadTemplateToS3(): Unit = {
-    // disable chunked encoding to work around https://github.com/jubos/fake-s3/issues/164
+  val templatesBucket = "ovo-comms-templates"
 
+  override def beforeAll(): Unit = {
+    super.beforeAll()
     s3Client.createBucket("ovo-comms-templates")
     s3Client.createBucket("dev-ovo-comms-pdfs")
+    uploadTemplateToS3(orchestratedPrintEvent.metadata.commManifest, s3Client, templatesBucket)
+  }
 
-    // template
-    s3Client.putObject("ovo-comms-templates",
-                       "service/composer-service-test/0.1/email/subject.txt",
-                       "SUBJECT {{profile.firstName}}")
-    s3Client.putObject("ovo-comms-templates",
-                       "service/composer-service-test/0.1/email/body.html",
-                       "{{> header}} HTML BODY {{amount}}")
-    s3Client.putObject("ovo-comms-templates",
-                       "service/composer-service-test/0.1/email/body.txt",
-                       "{{> header}} TEXT BODY {{amount}}")
-    s3Client.putObject("ovo-comms-templates",
-                       "service/composer-service-test/0.1/sms/body.txt",
-                       "{{> header}} SMS BODY {{amount}}")
-    s3Client.putObject("ovo-comms-templates",
-                       "service/composer-service-test/0.1/print/body.html",
-                       "Hello {{profile.firstName}}")
+  lazy val s3Client = {
+    val s3clientOptions = S3ClientOptions.builder().setPathStyleAccess(true).disableChunkedEncoding().build()
+    val s3: AmazonS3Client = new AmazonS3Client(new BasicAWSCredentials("service-test", "dummy"))
+      .withRegion(Regions.fromName(config.getString("aws.region")))
+    s3.setS3ClientOptions(s3clientOptions)
+    s3.setEndpoint(s3Endpoint)
+    s3
+  }
 
-    // fragments
-    s3Client.putObject("ovo-comms-templates", "service/fragments/email/html/header.html", "HTML HEADER")
-    s3Client.putObject("ovo-comms-templates", "service/fragments/email/txt/header.txt", "TEXT HEADER")
-    s3Client.putObject("ovo-comms-templates", "service/fragments/sms/txt/header.txt", "SMS HEADER")
+  it should "create a ComposedPrint event, for a valid request, archiving the rendered pdf in s3" in {
+    createOKDocRaptorResponse()
+    withThrowawayConsumerFor(topics.composedPrint.v1) { consumer =>
+      topics.orchestratedPrint.v1.publishOnce(orchestratedPrintEvent, 10.seconds)
+      val composedPrintEvent = consumer.pollFor(noOfEventsExpected = 1).head
+      val key =
+        s"composer-service-test/${LocalDateTime.ofInstant(now, ZoneId.systemDefault()).format(dateFormatter)}/${now.toEpochMilli}-${orchestratedPrintEvent.metadata.traceToken}-${orchestratedPrintEvent.internalMetadata.internalTraceToken}.pdf"
+
+      composedPrintEvent.metadata.traceToken shouldBe orchestratedPrintEvent.metadata.traceToken
+      composedPrintEvent.internalMetadata.internalTraceToken shouldBe orchestratedPrintEvent.internalMetadata.internalTraceToken
+      composedPrintEvent.pdfIdentifier should be(key)
+      val s3 = s3Client
+
+      val files: ObjectListing = s3.listObjects("dev-ovo-comms-pdfs")
+      files.getObjectSummaries.asScala.foreach(println)
+
+      val s3Object = s3.getObject("dev-ovo-comms-pdfs", key)
+      IOUtils.toByteArray(s3Object.getObjectContent) should contain theSameElementsAs (pdfResponseByteArray)
+    }
+  }
+
+  it should "send a failed event if docRaptor is not available" in {
+    create400DocRaptorResponse()
+    withThrowawayConsumerFor(topics.failed.v2) { consumer =>
+      topics.orchestratedPrint.v1.publishOnce(orchestratedPrintEvent, 10.seconds)
+      val failedEvent = consumer.pollFor(noOfEventsExpected = 1).head
+
+      failedEvent.errorCode should be(CompositionError)
+      failedEvent.reason should be("Failed to render pdf: Bad Request")
+    }
   }
 
   def createOKDocRaptorResponse() {
@@ -184,5 +158,4 @@ class PrintServiceTest
           .withBody("<error>Problemo</error>")
       )
   }
-
 }
