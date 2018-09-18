2f385af4f08ac6627aa451485783affd7568cea0
Fabio Labella <fabio.labella2@gmail.com>
2018-09-18 16:53:41 +0100

    Fragment typeclass

--- src/main/scala/com/ovoenergy/comms/composer/v2/Sms.scala
+++ src/main/scala/com/ovoenergy/comms/composer/v2/Sms.scala
@@ -10,21 +10,30 @@ import com.ovoenergy.comms.model.sms.{ComposedSMSV4, OrchestratedSMSV3}
 
 object Program {
 
-  def apply[F[_] : FlatMap](event: OrchestratedSMSV3)(implicit rendering: Rendering[F], store: Store[F], templates: Templates[F, Templates.Sms], hash: Hash[F], time: Time[F]): F[ComposedSMSV4] = {
+  def apply[F[_]: FlatMap](event: OrchestratedSMSV3)(implicit rendering: Rendering[F],
+                                                     store: Store[F],
+                                                     templates: Templates[F, Templates.Sms],
+                                                     hash: Hash[F],
+                                                     time: Time[F]): F[ComposedSMSV4] = {
     for {
       template <- templates.get(event.metadata.templateManifest)
       now <- time.now
-      renderedSms <- rendering.renderSms(now, event.metadata.templateManifest, template, SMSTemplateData(event.templateData, event.customerProfile, event.recipientPhoneNumber))
+      renderedSms <- rendering.renderSms(
+        now,
+        event.metadata.templateManifest,
+        template,
+        SMSTemplateData(event.templateData, event.customerProfile, event.recipientPhoneNumber))
       bodyUri <- store.upload(event.metadata.commId, event.metadata.traceToken, renderedSms.textBody)
       eventId <- hash(event.metadata.eventId)
       hashedComm <- hash(event)
-    } yield ComposedSMSV4(
-      metadata = MetadataV3.fromSourceMetadata("comms-composer", event.metadata, eventId),
-      internalMetadata = event.internalMetadata,
-      recipient = event.recipientPhoneNumber,
-      textBody = bodyUri.renderString,
-      expireAt = event.expireAt,
-      hashedComm = hashedComm
-    )
+    } yield
+      ComposedSMSV4(
+        metadata = MetadataV3.fromSourceMetadata("comms-composer", event.metadata, eventId),
+        internalMetadata = event.internalMetadata,
+        recipient = event.recipientPhoneNumber,
+        textBody = bodyUri.renderString,
+        expireAt = event.expireAt,
+        hashedComm = hashedComm
+      )
   }
 }
