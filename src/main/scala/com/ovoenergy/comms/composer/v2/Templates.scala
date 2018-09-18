ca000afd4cfb51ace21ae0a64d25afa3daf11a51
Zsolt Balvanyos <zsolt.balvanyos@ovoenergy.com>
2018-09-18 14:53:18 +0100

    Implementing the Templates interface

--- src/main/scala/com/ovoenergy/comms/composer/v2/Templates.scala
+++ src/main/scala/com/ovoenergy/comms/composer/v2/Templates.scala
@@ -1,9 +1,13 @@
 package com.ovoenergy.comms.composer.v2
 
 import cats.Id
+import cats.data.Validated
+import cats.data.Validated.{Invalid, Valid}
+import cats.effect.Effect
 import com.ovoenergy.comms.model.TemplateManifest
+import com.ovoenergy.comms.templates.{TemplatesContext, TemplatesRepo}
 import com.ovoenergy.comms.templates.model.template.{processed => templates}
-import com.ovoenergy.comms.templates.model.EmailSender
+import com.ovoenergy.comms.templates.model.template.processed.CommTemplate
 
 trait Templates[F[_], Template] {
   def get(manifest: TemplateManifest): F[Template]
@@ -14,7 +18,32 @@ object Templates {
   type Print = templates.print.PrintTemplate[Id]
   type Sms = templates.sms.SMSTemplate[Id]
 
-  def sms[F[_]]: Templates[F, Templates.Sms] = ???
-  def email[F[_]]: Templates[F, Templates.Email] = ???
-  def print[F[_]]: Templates[F, Templates.Print] = ???
+  def sms[F[_]](implicit F: Effect[F], templatesContext: TemplatesContext): Templates[F, Templates.Sms] =
+    new Templates[F, Templates.Sms] {
+      override def get(manifest: TemplateManifest): F[Sms] = loadTemplate(manifest, _.sms)
+    }
+
+  def email[F[_]](implicit F: Effect[F], templatesContext: TemplatesContext): Templates[F, Templates.Email] =
+    new Templates[F, Templates.Email] {
+      override def get(manifest: TemplateManifest): F[Email] = loadTemplate(manifest, _.email)
+    }
+
+  def print[F[_]](implicit F: Effect[F], templatesContext: TemplatesContext): Templates[F, Templates.Print] =
+    new Templates[F, Templates.Print] {
+      override def get(manifest: TemplateManifest): F[Print] = loadTemplate(manifest, _.print)
+    }
+
+  def loadTemplate[F[_], A](manifest: TemplateManifest, f: CommTemplate[Id] => Option[A])(
+      implicit F: Effect[F],
+      templatesContext: TemplatesContext): F[A] = {
+    TemplatesRepo.getTemplate(templatesContext, manifest) match {
+      case Valid(commTemplate) =>
+        f(commTemplate) match {
+          case Some(t) => F.pure(t)
+          case None =>
+            F.raiseError(new RuntimeException(s"Template for channel not found")) // TODO: change to ComposerError
+        }
+      case Invalid(i) => F.raiseError(new RuntimeException(i.toList.mkString(","))) // TODO: change to ComposerError
+    }
+  }
 }
