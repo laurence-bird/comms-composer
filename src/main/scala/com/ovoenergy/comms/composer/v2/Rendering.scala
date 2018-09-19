7cd45d2dc2424c7b2ff6d9433865a9410619383b
Laurence Bird <laurence.bird@ovoenergy.com>
2018-09-19 14:25:25 +0100

    Rendering Implementation and unit tests

--- src/main/scala/com/ovoenergy/comms/composer/v2/Rendering.scala
+++ src/main/scala/com/ovoenergy/comms/composer/v2/Rendering.scala
@@ -3,7 +3,7 @@ package v2
 
 import java.time.ZonedDateTime
 
-import cats.{Apply, MonadError}
+import cats.Apply
 import cats.data.Validated
 import cats.effect.Sync
 import cats.implicits._
@@ -34,16 +34,20 @@ object Rendering {
 
   def fileName(channel: Channel, manifest: TemplateManifest, suffixes: String*) =
     (Seq(S3Prefix.fromTemplateManifest(manifest), channel.toString) ++ suffixes).mkString("::")
-  def apply[F[_]](handlebars: HtmlRendering, pdfRendering: PdfRendering[F])(implicit F: Sync[F]) =
+
+  def apply[F[_]](handlebars: HandlebarsRendering)(implicit F: Sync[F]) =
     new Rendering[F] {
       override def renderEmail(time: ZonedDateTime,
                                manifest: TemplateManifest,
                                template: Email,
                                emailTemplateData: CommTemplateData): F[Email.Rendered] = {
-        val subject = handlebars.render(template.subject, time, emailTemplateData, ???)
-        val htmlBody = handlebars.render(template.htmlBody, time, emailTemplateData, ???)
+        val channel = EmailChan
+        val subject =
+          handlebars.render(template.subject, time, emailTemplateData, fileName(channel, manifest, "subject"))
+        val htmlBody =
+          handlebars.render(template.htmlBody, time, emailTemplateData, fileName(channel, manifest, "htmlBody"))
         val textBody =
-          template.textBody.map(handlebars.render(_, time, emailTemplateData, ???))
+          template.textBody.map(handlebars.render(_, time, emailTemplateData, fileName(channel, manifest, "textBody")))
 
         val errorsOrResult: Validated[Errors, Email.Rendered] =
           Apply[Validated[Errors, ?]].map3(subject, htmlBody, textBody.sequence[Validated[Errors, ?], String]) {
@@ -60,7 +64,8 @@ object Rendering {
                              template: Sms,
                              smsTemplateData: CommTemplateData): F[SMS.Rendered] = {
 
-        val textBody = handlebars.render(template.textBody, time, smsTemplateData, ???)
+        val textBody =
+          handlebars.render(template.textBody, time, smsTemplateData, fileName(SmsChan, manifest, "textBody"))
 
         F.fromEither {
           textBody
@@ -79,7 +84,7 @@ object Rendering {
             template.body,
             time,
             printTemplateData,
-            ???
+            fileName(PrintChan, manifest, "htmlBody")
           )
           .map(renderedHtmlBody => Print.HtmlBody(renderedHtmlBody))
 
@@ -90,6 +95,7 @@ object Rendering {
         }
       }
 
-      override def renderPrintPdf(html: Print.HtmlBody): F[Print.RenderedPdf] = pdfRendering.apply(html)
+      override def renderPrintPdf(html: Print.HtmlBody): F[Print.RenderedPdf] = ???
+
     }
 }
