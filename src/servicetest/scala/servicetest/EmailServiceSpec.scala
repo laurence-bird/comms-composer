5bae6a0b00e3dc1bce84588250814cee6077084e
Filippo De Luca <filippo.deluca@ovoenergy.com>
2018-10-02 09:31:24 +0100

    Add email failed service test

--- src/servicetest/scala/servicetest/EmailServiceSpec.scala
+++ src/servicetest/scala/servicetest/EmailServiceSpec.scala
@@ -14,7 +14,7 @@ import org.apache.kafka.clients.producer.ProducerRecord
 class EmailServiceSpec extends ServiceSpec with TestGenerators {
 
   "Composer" should {
-    "process orchestrated email message" in {
+    "process orchestrated email message successfully" in {
 
       val sourceMessage = {
         val initial = generate[OrchestratedEmailV4]
@@ -41,6 +41,38 @@ class EmailServiceSpec extends ServiceSpec with TestGenerators {
       }.futureValue
 
       consumed.value().metadata.commId shouldBe sourceMessage.metadata.commId
+      consumed.value().recipient shouldBe sourceMessage.recipientEmailAddress
+    }
+
+    "send a feedback (and failed) message if the template does not exist" in {
+
+      val sourceMessage = {
+        val initial = generate[OrchestratedEmailV4]
+        initial.copy(
+          customerProfile = generate[CustomerProfile].some,
+          templateData = Map("amount"->TemplateData.fromString("10000"))
+        )
+      }
+
+      val (failed, feedback) = withProducerFor(topics.orchestratedEmail) { producer =>
+        for {
+          _ <- kafka.produceRecord[IO](
+            producer,
+            new ProducerRecord[String, OrchestratedEmailV4](
+              topics.orchestratedEmail.name,
+              sourceMessage.metadata.commId,
+              sourceMessage)
+          )
+          failedRecord <- consume(topics.failed)(r => r.pure[IO]).head.compile.lastOrRethrow
+          feedbackRecord <- consume(topics.feedback)(r => r.pure[IO]).head.compile.lastOrRethrow
+        } yield (failedRecord, feedbackRecord)
+      }.futureValue
+
+      failed.value().metadata.commId shouldBe sourceMessage.metadata.commId
+      failed.value().errorCode shouldBe TemplateDownloadFailed
+
+      feedback.value().commId shouldBe sourceMessage.metadata.commId
+      feedback.value().status shouldBe FeedbackOptions.Failed
     }
   }
 
