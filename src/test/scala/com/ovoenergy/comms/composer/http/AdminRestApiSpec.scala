892e2ebfcf152148af3fae67835f2fb6b53f989c
Zsolt Balvanyos <zsolt.balvanyos@ovoenergy.com>
2019-01-04 15:59:45 +0000

    Tests passing

--- src/test/scala/com/ovoenergy/comms/composer/http/AdminRestApiSpec.scala
+++ src/test/scala/com/ovoenergy/comms/composer/http/AdminRestApiSpec.scala
@@ -6,6 +6,7 @@ import cats.effect.IO
 import org.scalatest.{FlatSpec, Matchers}
 
 import org.http4s._
+import org.http4s.implicits._
 import org.http4s.client.dsl.Http4sClientDsl
 import org.http4s.dsl.Http4sDsl
 
@@ -16,24 +17,20 @@ class AdminRestApiSpec
     with Http4sDsl[IO]
     with Http4sClientDsl[IO] {
 
-  private val service = AdminRestApi[IO].adminService.orNotFound
+  private val service = AdminRestApi[IO].adminService
 
   "/health" should "return HTTP 200 when the service is healthy" in {
-    val response = (for {
-      request <- GET(Uri.uri("/health"))
-      response <- service.run(request)
-    } yield response).futureValue
-
-    response.status shouldBe Ok
+    service.orNotFound
+      .run(Request(method = Method.GET, uri = Uri.uri("/health")))
+      .futureValue
+      .status shouldBe Ok
   }
 
   "/foo" should "return HTTP 404" in {
-    val response = (for {
-      request <- GET(Uri.uri("/foo"))
-      response <- service.run(request)
-    } yield response).futureValue
-
-    response.status shouldBe NotFound
+    service.orNotFound
+      .run(Request(method = Method.GET, uri = Uri.uri("/foo")))
+      .futureValue
+      .status shouldBe NotFound
   }
 
 }
--- src/test/scala/com/ovoenergy/comms/composer/http/RenderRestApiSpec.scala
+++ src/test/scala/com/ovoenergy/comms/composer/http/RenderRestApiSpec.scala
@@ -1,23 +1,23 @@
 package com.ovoenergy.comms.composer
 package http
 
+import java.net.URI
+import java.nio.charset.StandardCharsets
+
 import http.RenderRestApi.{Render, RenderRequest}
 import model.ComposerError
-import model.Print.{RenderedPdf, PdfBody}
-
+import model.Print.{PdfBody, RenderedPdf}
 import com.ovoenergy.comms.model._
-
 import io.circe.syntax._
 import io.circe.parser._
 import io.circe.literal._
 import cats.implicits._
 import cats.effect.IO
-
 import org.http4s._
+import org.http4s.implicits._
 import org.http4s.circe._
 import org.http4s.client.dsl.Http4sClientDsl
 import org.http4s.dsl.Http4sDsl
-
 import org.scalatest.{FlatSpec, Matchers}
 
 class RenderRestApiSpec
@@ -36,106 +36,98 @@ class RenderRestApiSpec
     buildRenderPrintF(response.pure[IO])
   }
 
+  def getBody(fields: Map[String, TemplateData]) =
+    fs2.Stream
+      .emit(RenderRequest(fields).asJson)
+      .flatMap(json => fs2.Stream.emits(json.noSpaces.getBytes(StandardCharsets.UTF_8).toSeq))
+      .covary[IO]
+
   private val successfulRender = buildRenderPrint(RenderedPdf(PdfBody("Hi".getBytes)))
 
   it should "return a valid response containing renderedPrintPDF if rendering is successful" in {
-
-    val service = new RenderRestApi[IO](successfulRender).renderService.orNotFound
-
-    val req: IO[Request[IO]] = POST(
-      uri("/yolo/1.0/Service/print"),
-      RenderRequest(
-        Map("Foo" -> TemplateData.fromString("bar"))
-      ).asJson
-    )
-
-    val response = req.flatMap(service.apply).futureValue
-    response.status shouldBe Ok
+    new RenderRestApi[IO](successfulRender).renderService.orNotFound
+      .run(
+        Request(
+          method = Method.POST,
+          uri = Uri.uri("/yolo/1.0/Service/print"),
+          body = getBody(Map("Foo" -> TemplateData.fromString("bar")))
+        ))
+      .futureValue
+      .status shouldBe Ok
   }
 
   it should "return an appropriate error if invalid comm type is passed in URL" in {
-
-    val service = new RenderRestApi[IO](successfulRender).renderService.orNotFound
-
-    val req: IO[Request[IO]] = POST(
-      uri("/yolo/1.0/invalid/print"),
-      RenderRequest(
-        Map("Foo" -> TemplateData.fromString("bar"))
-      ).asJson
-    )
-
-    val response = req.flatMap(service.apply).futureValue
-    response.status shouldBe NotFound
+    new RenderRestApi[IO](successfulRender).renderService.orNotFound
+      .run(
+        Request(
+          method = POST,
+          uri = Uri.uri("/yolo/1.0/invalid/print"),
+          body = getBody(Map("Foo" -> TemplateData.fromString("bar")))
+        )
+      )
+      .futureValue
+      .status shouldBe NotFound
   }
 
   it should "return an appropriate error if JSON deserialisation fails" in {
-
-    val service = new RenderRestApi[IO](successfulRender).renderService.orNotFound
-
-    val invalidJson = parse("""{
-      |"invalidBody": "yooo"
-      |}
-    """.stripMargin).right.get
-
-    val req: IO[Request[IO]] = POST(
-      uri("/yolo/1.0/Service/print"),
-      json"""{"invalidBody": "yooo"}"""
-    )
-
-    val response = req.flatMap(service.apply).futureValue
-    response.status shouldBe BadRequest
+    new RenderRestApi[IO](successfulRender).renderService.orNotFound
+      .run(
+        Request(
+          method = Method.POST,
+          uri = Uri.uri("/yolo/1.0/Service/print"),
+          body = fs2.Stream
+            .emits("""{"invalidBody": "yooo"}""".getBytes(StandardCharsets.UTF_8).toSeq)
+            .covary[IO]
+        )
+      )
+      .futureValue
+      .status shouldBe BadRequest
   }
 
   // TODO It should not really been NotFound if S3 is down for example
   it should "return NotFound if the template is missing" in {
-
-    val service = new RenderRestApi[IO](buildRenderPrintF(IO.raiseError(
+    new RenderRestApi[IO](buildRenderPrintF(IO.raiseError(
       ComposerError("Template download failed", TemplateDownloadFailed)))).renderService.orNotFound
-
-    val req: IO[Request[IO]] = POST(
-      uri("/yolo/1.0/Service/print"),
-      RenderRequest(
-        Map("Foo" -> TemplateData.fromString("bar"))
-      ).asJson
-    )
-
-    val response = req.flatMap(service.apply).futureValue
-    response.status shouldBe NotFound
+      .run(
+        Request(
+          method = Method.POST,
+          uri = Uri.uri("/yolo/1.0/Service/print"),
+          body = getBody(Map("Foo" -> TemplateData.fromString("bar")))
+        )
+      )
+      .futureValue
+      .status shouldBe NotFound
   }
 
   it should "return UnprocessableEntity if the template data is incomplete" in {
-
-    val service = new RenderRestApi[IO](
+    new RenderRestApi[IO](
       buildRenderPrintF(
         IO.raiseError(ComposerError(
           "Missing fields from template data: yo, lo",
           MissingTemplateData)))).renderService.orNotFound
-
-    val req: IO[Request[IO]] = POST(
-      uri("/yolo/1.0/Service/print"),
-      RenderRequest(
-        Map("Foo" -> TemplateData.fromString("bar"))
-      ).asJson
-    )
-
-    val response = req.flatMap(service.apply).futureValue
-    response.status shouldBe UnprocessableEntity
+      .run(
+        Request(
+          method = Method.POST,
+          uri = Uri.uri("/yolo/1.0/Service/print"),
+          body = getBody(Map("Foo" -> TemplateData.fromString("bar")))
+        )
+      )
+      .futureValue
+      .status shouldBe UnprocessableEntity
   }
 
   it should "return InternalServerError if the template data is incomplete" in {
-
-    val service = new RenderRestApi[IO](buildRenderPrintF(IO.raiseError(
+    new RenderRestApi[IO](buildRenderPrintF(IO.raiseError(
       ComposerError("Something really wrong", CompositionError)))).renderService.orNotFound
-
-    val req: IO[Request[IO]] = POST(
-      uri("/yolo/1.0/Service/print"),
-      RenderRequest(
-        Map("Foo" -> TemplateData.fromString("bar"))
-      ).asJson
-    )
-
-    val response = req.flatMap(service.apply).futureValue
-    response.status shouldBe InternalServerError
+      .run(
+        Request(
+          method = Method.POST,
+          uri = Uri.uri("/yolo/1.0/Service/print"),
+          body = getBody(Map("Foo" -> TemplateData.fromString("bar")))
+        )
+      )
+      .futureValue
+      .status shouldBe InternalServerError
   }
 
 }
